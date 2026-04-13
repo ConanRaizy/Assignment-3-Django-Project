e491701bcca2f070cef36b2ba712fe1c1cdecd2b
ConanRaizy <conantheprod@gmail.com>
2026-04-13 11:39:47 -0400

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
+from django.urls import path
 
+from . import views
+
+app_name = "polls"
 urlpatterns = [
-    path("", RedirectView.as_view(url="/polls/"), permanent=False),
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
